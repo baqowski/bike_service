e7b72d66b4833b159bee3587870c59793186f2a3
Baq <baqu1208@gmail.com>
2020-07-30 20:39:31 +0200

    first commit

---
+++ src/main/java/app/BikeAppApplication.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BikeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/bike_app/BikeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package app.bike_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
