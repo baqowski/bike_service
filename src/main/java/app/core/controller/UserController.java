9cc667ddae3e48b378076a4ab31e3f9c72415dbe
Baq <baqu1208@gmail.com>
2020-09-03 00:48:48 +0200

    Refactoring model

--- src/main/java/app/core/controller/UserController.java
+++ src/main/java/app/core/controller/UserController.java
@@ -1,14 +1,9 @@
 package app.core.controller;
 
-import app.core.entity.Order;
-import app.core.entity.User;
-import app.core.repository.UserRepository;
 import app.core.service.UserService;
-import javassist.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 /**
  * @author Karol Bąk
